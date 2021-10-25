c8f734e2fcf7a5cf094e5c2edf985f88ab09e6ca
Jay ashworth <washwor1@vols.utk.edu>
2021-10-24 21:36:29 -0400

    bruh

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,8 +1,36 @@
 // dijsktras.cpp
 
 // Main Execution
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
 
 int main(int argc, char *argv[]) {
+    int numInputs, intInput;
+    char charInput;
+    map<char,int> values;
+    vector<char> board;
+    pair<int,int> boardSize, start, finish;
+
+    cin >> numInputs;
+    //fills a map with the values of each character that will be in the grid
+    for(int i =0; i<numInputs; i++) {
+        cin >> charInput >> intInput;
+        values.insert(make_pair(charInput,intInput));
+    }
+
+    cin >> boardSize.first >> boardSize.second;
+
+    //creates the grid
+    for(int i = 0; i < boardSize.first * boardSize.second; i++) {
+        cin >> charInput;
+        board.push_back(charInput);
+    }
+
+    cin >> start.first >> start.second >> finish.first >> finish.second;
+    
     return 0;
 }
 
