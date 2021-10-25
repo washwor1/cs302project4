9a3886d055603d29d29ca22518a73ea298a3c359
Jay ashworth <washwor1@vols.utk.edu>
2021-10-24 23:18:54 -0400

    started djikstras

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -15,6 +15,8 @@ int main(int argc, char *argv[]) {
     char charInput;
     map<char,int> values;
     vector<char> board;
+    vector<bool> visited;
+    vector<int> backedge, distance;
     pair<int,int> boardSize, start, finish;
     multimap<int,int> nodesToAdd;
 
@@ -31,9 +33,31 @@ int main(int argc, char *argv[]) {
     for(int i = 0; i < boardSize.first * boardSize.second; i++) {
         cin >> charInput;
         board.push_back(charInput);
+        visited.push_back(false);
+        backedge.push_back(NULL);
+        distance.push_back(-1);
     }
 
     cin >> start.first >> start.second >> finish.first >> finish.second;
+
+    //sets the first node to be the start and runs the first round of dijkstras to do specialty for first node.
+    nodesToAdd.insert(make_pair(0, (boardSize.second*start.second+start.first)));
+    multimap<int,int>::iterator it = nodesToAdd.begin();
+    visited.at(it->second) = true;
+    distance.at(it->second) = 0;
+    if (it->second&boardSize.second>0) {
+        
+    }
+    
+
+
+    //begin main dijkstras 
+    //iteratively scans through each node; updating distances, backedges, the multimap, and visited status.
+    for(int i = 0; i<boardSize.first*boardSize.second-1; i++) {
+        visited.at(nodesToAdd.begin()->second) = true;
+        back
+    }
+
     
     return 0;
 }
